7ee6456d5389ae286ce7a0627f919a8116ae3245
Andrea Orlandi <andrea.orlandi@brainstorm.it>
2016-07-20 22:03:53 +0200

    init

---
+++ _9_Led_1_alla_volta_no_dly/_9_Led_1_alla_volta_no_dly.ino
@@ -0,0 +1,87 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin 13. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://www.arduino.cc
+
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+ */
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+   digitalWrite(2, HIGH);   
+  delay(1000);            
+  digitalWrite(2, LOW); 
+  delay(1000);
+  
+  digitalWrite(2, HIGH);   
+  delay(250);            
+  digitalWrite(2, LOW);    
+  
+  
+   digitalWrite(3, HIGH);   
+  delay(250);            
+  digitalWrite(3, LOW);    
+    
+  
+   digitalWrite(4, HIGH);   
+  delay(250);            
+  digitalWrite(4, LOW);    
+   
+  
+   digitalWrite(5, HIGH);   
+  delay(250);            
+  digitalWrite(5, LOW);    
+    
+  
+   digitalWrite(6, HIGH);   
+  delay(250);            
+  digitalWrite(6, LOW);    
+    
+  
+  digitalWrite(7, HIGH);   
+  delay(250);            
+  digitalWrite(7, LOW);    
+    
+   
+  digitalWrite(8, HIGH);   
+  delay(250);            
+  digitalWrite(8, LOW);    
+    
+  
+  digitalWrite(9, HIGH);   
+  delay(250);            
+  digitalWrite(9, LOW);    
+   
+  
+  digitalWrite(10, HIGH);   
+  delay(250);            
+  digitalWrite(10, LOW);    
+  delay(250);
+
+  digitalWrite(10, HIGH);   
+  delay(1000);            
+  digitalWrite(10, LOW);    
+  delay(1000);
+}
